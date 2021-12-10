8a2bf17a9027ea07b8ff24531b73f7e6e928546f
Richard Havalda <havalda.r94@gmail.com>
2021-12-10 18:52:30 +0100

    feat: apply first example component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './components/learning-examples/FirstComponent';
 
 class App extends Component {
   render() {
-    return <div className='App'>My Hello World!</div>;
+    return (
+      <div className='App'>
+        <FirstComponent />
+      </div>
+    );
   }
 }
 export default App;
