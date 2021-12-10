04c85046bf5d5c50555ca17f4b2b11f30303dcff
Richard Havalda <havalda.r94@gmail.com>
2021-12-10 20:12:55 +0100

    fix: remove unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent';
+//import FirstComponent from './components/learning-examples/FirstComponent';
 import Counter from './components/counter/Counter';
 
 class App extends Component {
