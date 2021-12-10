77b4760c434de82d1fa018912c5fe83112df4e09
Richard Havalda <havalda.r94@gmail.com>
2021-12-10 19:02:14 +0100

    feat: initialize counter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './components/learning-examples/FirstComponent';
+import Counter from './components/counter/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className='LearningComponents'>
         <FirstComponent />
       </div>
     );
   }
 }
+
 export default App;
