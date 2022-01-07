fb8389d62e6e04ae4fea37857d79749677d4b744
Richard Havalda <havalda.r94@gmail.com>
2022-01-07 10:10:14 +0100

    feat: add state to login

--- src/components/todo/ToDoApp.js
+++ src/components/todo/ToDoApp.js
@@ -11,11 +11,41 @@ export default class ToDoApp extends Component {
 }
 
 class LoginComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'in28minutes',
+      password: '',
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        Username: <input type='text' name='username' />
-        Password: <input type='password' name='password' />
+        Username:{' '}
+        <input
+          type='text'
+          name='username'
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        Password:{' '}
+        <input
+          type='password'
+          name='password'
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
         <button>LOGIN</button>
       </div>
     );
