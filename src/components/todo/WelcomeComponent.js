9b580cb5c7a5ffbb59e8fabc8512d542d911c62e
Richard Havalda <havalda.r94@gmail.com>
2022-01-14 12:25:33 +0100

    feat: add link to todos

--- src/components/todo/WelcomeComponent.js
+++ src/components/todo/WelcomeComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class WelcomeComponent extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome {this.props.loginName} to Todo APP</h1>
+        <h1>
+          Welcome {this.props.loginName}! You can manage your todos{' '}
+          <Link to='/todos'>here!</Link>
+        </h1>
       </div>
     );
   }
