20ea4e97822dc67fef1fd25ccd075791b906ac2c
Richard Havalda <havalda.r94@gmail.com>
2022-01-14 13:44:33 +0100

    feat: enhance footer component

--- src/components/todo/FooterComponent.js
+++ src/components/todo/FooterComponent.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class FooterComponent extends Component {
   render() {
     return (
-      <div>
-        <hr />
-        Footer
-      </div>
+      <footer className='footer'>
+        <span className='text-muted'>
+          All rights reserved 2022 @in28minutes
+        </span>
+      </footer>
     );
   }
 }
