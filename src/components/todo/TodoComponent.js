818cf13ad9cdec8ab360ad4e1cafcd2d1e5a56c0
Richard Havalda <havalda.r94@gmail.com>
2022-01-15 15:46:47 +0100

    feat: add form validation

--- src/components/todo/TodoComponent.js
+++ src/components/todo/TodoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 export class TodoComponent extends Component {
   constructor(props) {
@@ -12,10 +12,21 @@ export class TodoComponent extends Component {
       targetDate: moment(new Date()).format('YYYY-MM-DD'),
     };
     this.onSubmit = this.onSubmit.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
   onSubmit(values) {
     console.log(values);
+  }
+
+  validate(values) {
+    let errors = {};
+    if (!values.description) errors.description = 'Enter a description!';
+    else if (values.description.length < 5)
+      errors.description = 'Description must be at least 5 characters!';
+    if (!moment(values.targetDate).isValid())
+      errors.targetDate = 'Enter a valid target date!';
+    return errors;
   }
 
   render() {
@@ -28,9 +39,22 @@ export class TodoComponent extends Component {
           <Formik
             initialValues={{ description: description, targetDate: targetDate }}
             onSubmit={this.onSubmit}
+            validate={this.validate}
+            validateOnChange={false}
+            validateOnBlur={false}
           >
             {(props) => (
               <Form>
+                <ErrorMessage
+                  name='description'
+                  component='div'
+                  className='alert alert-warning'
+                />
+                <ErrorMessage
+                  name='targetDate'
+                  component='div'
+                  className='alert alert-warning'
+                />
                 <fieldset className='form-group'>
                   <label>Description</label>
                   <Field
