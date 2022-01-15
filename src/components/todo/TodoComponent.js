5b41443cbc51d6d77f7a91c8c5e35f790dbfeb48
Richard Havalda <havalda.r94@gmail.com>
2022-01-15 15:36:02 +0100

    feat: add form with formik and moment

--- src/components/todo/TodoComponent.js
+++ src/components/todo/TodoComponent.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
 import { useParams } from 'react-router-dom';
+import moment from 'moment';
+import { Field, Form, Formik } from 'formik';
 
 export class TodoComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 1,
+      description: 'Learn forms',
+      targetDate: moment(new Date()).format('YYYY-MM-DD'),
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
-    return <div>Todo component - {this.props.id}</div>;
+    let { description, targetDate } = this.state;
+    // let targetDate = this.state.targetDate;
+    return (
+      <div>
+        <h1>Todo</h1>
+        <div className='container'>
+          <Formik
+            initialValues={{ description: description, targetDate: targetDate }}
+            onSubmit={this.onSubmit}
+          >
+            {(props) => (
+              <Form>
+                <fieldset className='form-group'>
+                  <label>Description</label>
+                  <Field
+                    className='form-control'
+                    type='text'
+                    name='description'
+                  />
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label>Target date</label>
+                  <Field
+                    className='form-control'
+                    type='date'
+                    name='targetDate'
+                  />
+                </fieldset>
+                <button className='btn btn-success' type='submit'>
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    );
   }
 }
 
